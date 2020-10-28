cfbe9c7d8d40f5d3218773e6e79809d841248473
Moumita Ray <moumitacloud08@gmail.com>
2020-10-29 00:06:47 +0530

    Eureka Server client setup

--- src/main/java/com/samplemicroservice/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/samplemicroservice/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.samplemicroservice.moviecatalogservice.resources;
 import com.samplemicroservice.moviecatalogservice.models.CatalogItem;
 import com.samplemicroservice.moviecatalogservice.models.Movie;
 import com.samplemicroservice.moviecatalogservice.models.Rating;
+import com.samplemicroservice.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,20 +29,14 @@ public class MovieCatalogResource {
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234",4),
-                new Rating("5643",5)
-        );
-        return ratings.stream().map(rating ->
+//        List<Rating> ratings = Arrays.asList(
+//                new Rating("1234",4),
+//                new Rating("5643",5)
+//        );
+        UserRating ratings =  restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId,UserRating.class);
+        return ratings.getRatings().stream().map(rating ->
         {
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
-            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(),"Science Fiction",rating.getRating());
         }).collect(Collectors.toList());
 
